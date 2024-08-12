9dd29aecdc83380ec9ffcc097d28b70e685cbd80
Adrian <kerpador@gmail.com>
2024-08-11 22:47:10 -0600

    aded

--- algorithms/sieve_of_Erathosnes/Sieve.f90
+++ algorithms/sieve_of_Erathosnes/Sieve.f90
@@ -1,7 +1,21 @@
 program Sieve
     implicit none
-    integer ::N
-    N=10
-contains 
-integer function Sieve()
-    integer , intent(inout)
+    integer ::i,j
+    integer, parameter ::N=1000000
+    logical ,dimension(N)::a
+    a=.true.
+    a(1)=.false.
+    do i=2,int(sqrt(real(N)))
+        if(a(i)) then 
+            do j=i*i,N,i
+                a(j)=.false.
+            end do
+        end if
+    end do
+    print *,"Numeros primos hasta "   ,N,":"
+    do i=2,N 
+        if(a(i)) then 
+            print*,i
+        end if
+    end do
+end program Sieve
